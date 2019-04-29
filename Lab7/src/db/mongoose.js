53700dfef2e16047311dde77e67ef87b09d00316
DavReds <dav_rojas@hotmail.com>
2019-04-29 00:05:06 -0500

    Removed private content :sweat_smile:

--- Lab7/src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose')
-const connectionURL = 'mongodb+srv://Admin:AdmnP4ss@webdevclass-sdf0f.mongodb.net/clase?retryWrites=true'
-
-mongoose.connect(connectionURL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
