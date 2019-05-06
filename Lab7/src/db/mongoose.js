a09db2607a76e4dd082593686b74901c86b3d0ee
DavReds <dav_rojas@hotmail.com>
2019-05-06 18:26:26 -0500

    Added the frontend of thw final exam

--- Lab7/src/db/mongoose.js
+++ Lab7/src/db/mongoose.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-const connection = require('./connection.js')
+const connection = require('./connection.js') // module.exports.DB_URL = <Database URL>
 const connectionURL = connection.DB_URL
 
 mongoose.connect(connectionURL, {
